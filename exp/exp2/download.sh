5b5ae78ee8ef1fbfd2191e06814feb1035e36e85
the null <42580530+chenxinye@users.noreply.github.com>
2023-02-13 16:30:58 +0000

    Update download.sh

--- exp/exp2/download.sh
+++ exp/exp2/download.sh
@@ -7,22 +7,26 @@ then
         wget ftp://ftp.irisa.fr/local/texmex/corpus/siftsmall.tar.gz
         wget ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz
         wget http://ann-benchmarks.com/gist-960-euclidean.hdf5
-        if $2; then
-                tar -xf siftsmall.tar.gz
-                tar -xf sift.tar.gz
-                mv fashion-mnist-784-euclidean.hdf5 Euclidean_data/fashion-mnist-784-euclidean.hdf5
-                mv siftsmall Euclidean_data/siftsmall
-                mv sift Euclidean_data/sift
-                mv gist-960-euclidean.hdf5 Euclidean_data/gist-960-euclidean.hdf5
-                
+        if [[ ! -z "$2" ]] ; then
+                if $2; then
+                        tar -xf siftsmall.tar.gz
+                        tar -xf sift.tar.gz
+                        mv fashion-mnist-784-euclidean.hdf5 Euclidean_data/fashion-mnist-784-euclidean.hdf5
+                        mv siftsmall Euclidean_data/siftsmall
+                        mv sift Euclidean_data/sift
+                        mv gist-960-euclidean.hdf5 Euclidean_data/gist-960-euclidean.hdf5
+                fi
+        fi
 elif [ "$1" = 2 ]
 then
         echo "Download Angular datasets."
         wget http://ann-benchmarks.com/glove-100-angular.hdf5
         wget http://ann-benchmarks.com/deep-image-96-angular.hdf5
-        if $2; then
-                mv glove-100-angular.hdf5 Angular_data/glove-100-angular.hdf5
-                mv deep-image-96-angular.hdf5 Angular_data/deep-image-96-angular.hdf5
+        if [[ ! -z "$2" ]] ; then
+                if $2; then
+                        mv glove-100-angular.hdf5 Angular_data/glove-100-angular.hdf5
+                        mv deep-image-96-angular.hdf5 Angular_data/deep-image-96-angular.hdf5
+        fi
 elif [ "$1" = 0 ]
 then
         echo "Download all the experimental datasets."
@@ -32,17 +36,22 @@ then
         wget http://ann-benchmarks.com/gist-960-euclidean.hdf5
         wget http://ann-benchmarks.com/glove-100-angular.hdf5
         wget http://ann-benchmarks.com/deep-image-96-angular.hdf5
-        if $2; then
-                tar -xf siftsmall.tar.gz
-                tar -xf sift.tar.gz
-                mv fashion-mnist-784-euclidean.hdf5 Euclidean_data/fashion-mnist-784-euclidean.hdf5
-                mv siftsmall Euclidean_data/siftsmall
-                mv sift Euclidean_data/sift
-                mv gist-960-euclidean.hdf5 Euclidean_data/gist-960-euclidean.hdf5
-                mv glove-100-angular.hdf5 Angular_data/glove-100-angular.hdf5
-                mv deep-image-96-angular.hdf5 Angular_data/deep-image-96-angular.hdf5
+        if [[ ! -z "$2" ]] ; then
+                if $2; then
+                        tar -xf siftsmall.tar.gz
+                        tar -xf sift.tar.gz
+                        mv fashion-mnist-784-euclidean.hdf5 Euclidean_data/fashion-mnist-784-euclidean.hdf5
+                        mv siftsmall Euclidean_data/siftsmall
+                        mv sift Euclidean_data/sift
+                        mv gist-960-euclidean.hdf5 Euclidean_data/gist-960-euclidean.hdf5
+                        mv glove-100-angular.hdf5 Angular_data/glove-100-angular.hdf5
+                        mv deep-image-96-angular.hdf5 Angular_data/deep-image-96-angular.hdf5
+                fi
+        fi
 fi
 
-
-if $3; then
-        python hdf5_tonpy.py
+if [[ ! -z "$3" ]] ; then
+        if $3; then
+                python hdf5_tonpy.py
+        fi
+fi
